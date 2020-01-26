9a735a6360569d802eb23436b82e177099c57daa
Michael Luo <michael.luo0801@gmail.com>
2020-01-26 17:16:48 +1100

    add facade pattern

---
+++ facade/facade.ts
@@ -0,0 +1,39 @@
+import { Subsystem1, Subsystem2 } from './subsystem';
+
+/**
+ * The Facade class provides a simple interface to the complex logic of one or
+ * several subsystems. The Facade delegates the client requests to the
+ * appropriate objects within the subsystem. The Facade is also responsible for
+ * managing their lifecycle. All of this shields the client from the undesired
+ * complexity of the subsystem.
+ */
+export class Facade {
+  protected subsystem1: Subsystem1;
+
+  protected subsystem2: Subsystem2;
+
+  /**
+   * Depending on your application's needs, you can provide the Facade with
+   * existing subsystem objects or force the Facade to create them on its own.
+   */
+  constructor(subsystem1: Subsystem1 = null, subsystem2: Subsystem2 = null) {
+    this.subsystem1 = subsystem1 || new Subsystem1();
+    this.subsystem2 = subsystem2 || new Subsystem2();
+  }
+
+  /**
+   * The Facade's methods are convenient shortcuts to the sophisticated
+   * functionality of the subsystems. However, clients get only to a fraction
+   * of a subsystem's capabilities.
+   */
+  public operation(): string {
+    let result = "Facade initializes subsystems:\n";
+    result += this.subsystem1.operation1();
+    result += this.subsystem2.operation1();
+    result += "Facade orders subsystems to perform the action:\n";
+    result += this.subsystem1.operationN();
+    result += this.subsystem2.operationZ();
+
+    return result;
+  }
+}
---
+++ facade/main.ts
@@ -0,0 +1,26 @@
+import { Facade } from './facade';
+import { Subsystem1, Subsystem2 } from './subsystem';
+
+/**
+ * The client code works with complex subsystems through a simple interface
+ * provided by the Facade. When a facade manages the lifecycle of the subsystem,
+ * the client might not even know about the existence of the subsystem. This
+ * approach lets you keep the complexity under control.
+ */
+function clientCode(facade: Facade) {
+  // ...
+
+  console.log(facade.operation());
+
+  // ...
+}
+
+/**
+ * The client code may have some of the subsystem's objects already created. In
+ * this case, it might be worthwhile to initialize the Facade with these objects
+ * instead of letting the Facade create new instances.
+ */
+const subsystem1 = new Subsystem1();
+const subsystem2 = new Subsystem2();
+const facade = new Facade(subsystem1, subsystem2);
+clientCode(facade);
