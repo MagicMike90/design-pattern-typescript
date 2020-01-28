deacbf5008caa84b50ac765a31816dd5d88543bc
Michael Luo <michael.luo0801@gmail.com>
2020-01-28 20:25:26 +1100

    add proxy pattern

---
+++ proxy/subject.ts
@@ -0,0 +1,21 @@
+/**
+ * The Subject interface declares common operations for both RealSubject and the
+ * Proxy. As long as the client works with RealSubject using this interface,
+ * you'll be able to pass it a proxy instead of a real subject.
+ */
+export interface Subject {
+  request(): void;
+}
+
+/**
+ * The RealSubject contains some core business logic. Usually, RealSubjects are
+ * capable of doing some useful work which may also be very slow or sensitive -
+ * e.g. correcting input data. A Proxy can solve these issues without any
+ * changes to the RealSubject's code.
+ */
+
+export class RealSubject implements Subject {
+  public request(): void {
+    console.log("RealSubject: Handling request.");
+  }
+}
